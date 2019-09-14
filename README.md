193682bbc1694c98607030e315554c09595cf968
Caroline Shea <caroline@portalmedia.com>
2019-09-14 11:03:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Bike Shop Cart SPA
+
+This React project is a framework for building a bike shop cart system.
+
+## Getting Started
+
+* Run the project using `npm start`
+* Run the tests using `npm test`
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Bike Shop SPA</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
