f48d9a4ba9037c3ed15cb35afa90a4d03cf4c482
Caroline Shea <caroline@portalmedia.com>
2019-09-19 09:21:12 -0500

    cleanup

--- src/components/CartBody.js
+++ src/components/CartBody.js
@@ -4,10 +4,9 @@ import '../App.css';
 class CartBody extends Component {
 
 	render() {
-		let qty = 3;
 		return (
 			<div className="cartBody">
-				<h3>You have {qty} items in your cart</h3>
+				<h3>You have ? items in your cart</h3>
 				<div className="cartBodyContent">
 					{/* render cart products here */}
 				</div>
