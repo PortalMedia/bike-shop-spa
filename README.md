4f332eeb8125eb7f6804f23f2941d796c875c292
theschmocker <jacobcs1996@gmail.com>
2022-07-30 13:27:03 -0500

    update react (+dom/scripts); add prettier and format project; use function components

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This React project is a framework for building a bike shop cart system.
 
 ## Getting Started
 
-* Install project dependencies with `npm install`
-* Run the project with `npm start`
-* Run the tests with `npm test`
+- Install project dependencies with `npm install`
+- Run the project with `npm start`
+- Run the tests with `npm test`
--- src/components/CartBody.js
+++ src/components/CartBody.js
@@ -1,22 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
-class CartBody extends Component {
-
-	render() {
-		return (
-			<div className="cartBody">
-				<h3>You have ? items in your cart</h3>
-				<div className="cartBodyContent">
-					{/* render cart products here */}
-				</div>
-			</div>
-		);
-	}
-
-	renderCartProducts() {
-		return null;
-	}
+function CartBody() {
+	return (
+		<div className="cartBody">
+			<h3>You have ? items in your cart</h3>
+			<div className="cartBodyContent">{/* render cart products here */}</div>
+		</div>
+	);
 }
 
 export default CartBody;
